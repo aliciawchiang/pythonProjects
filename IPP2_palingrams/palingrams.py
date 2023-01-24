625efeb317a56bafc1f4efcb68de0eededa3db35
Alicia Chiang <aliciawchiang@gmail.com>
2023-01-24 10:40:42 -0800

    finished palingram

--- IPP2_palingrams/palingrams.py
+++ IPP2_palingrams/palingrams.py
@@ -10,6 +10,19 @@ def find_palingrams():
         rev_word = word[::-1]
         if end > 1:
             for i in range(end):
-                if word[i:] == rev_word[]
+                if word[i:] == rev_word[:end-i] and rev_word[end-i:] in word_list:
+                    palingram_list.append((word, rev_word[end-i:]))
+                if word[:i] == rev_word[end-i:] and rev_word[:end-i] in word_list:
+                    palingram_list.append((rev_word[:end-i], word))
+    return palingram_list
+
+palingrams = find_palingrams()
+# sort palingrams on first word
+palingrams_sorted = sorted(palingrams)
+
+# display list of palingrams
+print("\nNumber of palingrams = {}\n".format(len(palingrams_sorted)))
+for first, second in palingrams_sorted:
+    print("{} {}".format(first, second))
 
 
